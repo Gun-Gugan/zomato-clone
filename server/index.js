c7867178c20d76f59dc485e32cb209bc5e4f2c00
kali <kali@kali.kali>
2025-07-02 11:29:35 +0530

    Fix: added modified route for Render backtend

--- server/index.js
+++ server/index.js
@@ -12,7 +12,10 @@ dotenv.config();
 const app = express();
 
 // Configure CORS
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://zomato-clone-client.onrender.com'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
