3b3b1ab90a15fa75500aa67765f518ce8eed7ec0
kali <kali@kali.kali>
2025-07-04 06:29:46 +0530

    add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import RestaurantList from './pages/RestaurantList';
 import Register from './pages/Register';
@@ -15,19 +16,27 @@ import ForgotPassword from './pages/ForgotPassword';
 import ContactForm from './pages/ContactForm';
 import TermsOfService from './components/TermsOfService';
 import PrivacyPolicy from './components/PrivacyPolicy';
+import Preloader from './components/Preloader'; 
 
-import { useEffect } from 'react';
-
-// Wrap App content with this Layout component
 function Layout() {
   const location = useLocation();
   const hideFooterRoutes = ['/login', '/register'];
-
   const shouldHideFooter = hideFooterRoutes.includes(location.pathname);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => setLoading(false), 1000); 
+    return () => clearTimeout(timer);
+  }, [location.pathname]);
+
+  if (loading) {
+    return <Preloader />;
+  }
 
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Navbar always shown */}
       {!shouldHideFooter && <Navbar />}
 
       <main className="flex-grow">
@@ -39,35 +48,19 @@ function Layout() {
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/orders" element={<ProtectedRoute><Orders /></ProtectedRoute>} />
           <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-          <Route
-          path="/forgot-password"
-          element={
-            <ProtectedRoute>
-              <ForgotPassword />
-            </ProtectedRoute>
-          }
-        />
+          <Route path="/forgot-password" element={<ProtectedRoute><ForgotPassword /></ProtectedRoute>} />
           <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
           <Route path="/contact" element={<ContactForm />} />
           <Route path="/terms" element={<TermsOfService />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           <Route path="*" element={<NotFound />} />
-
         </Routes>
       </main>
 
-      {/* Footer only if not on login/register */}
       {!shouldHideFooter && <Footer />}
     </div>
   );
 }
 
-function App() {
-  return (
-    <Router>
-      <Layout />
-    </Router>
-  );
-}
+export default Layout;
 
-export default App;
